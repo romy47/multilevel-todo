704cbfdeca20ff55939e5246a7df3df962be7ef5
Sebastian <romy6047@gmail.com>
2020-11-02 13:39:06 -0600

    nav drawer added

--- lib/home.dart
+++ lib/home.dart
@@ -68,6 +68,40 @@ class HomeScreen extends StatelessWidget {
         tooltip: 'Add Todo',
         child: const Icon(Icons.add),
       ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('User Name'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Create Todo'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => CreateTodo()));
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 
